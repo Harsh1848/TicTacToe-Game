165ae805132527f610c274a1934dc804de737e7d
Harsh1848 <harsh.hp7999@gmail.com>
2023-08-18 20:31:33 -0400

    UI updated

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,71 +1,158 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using TicTacToe_Game.Models;
 
 namespace TicTacToe_Game.Controllers
-{ 
+{
     public class GameController : Controller
     {
+        private static Game _game = new Game();
+
         public IActionResult Index()
         {
-            var gameBoard = new GameBoard
-            {
-                Board = new char[3, 3],
-                CurrentPlayer = 'X'
-            };
-            
-            return View(gameBoard);
+            return View(_game);
         }
 
-        [HttpGet]
-        public IActionResult MakeMove(int row, int col)
+        [HttpPost]
+        public IActionResult MakeMove(int position)
         {
-            var gameBoard = new GameBoard();
+            _game.MakeMove(position);
 
-            if (TempData.ContainsKey("Board") && TempData["Board"] is char[,] board)
+            if (_game.IsComputerPlayer && _game.Winner == null)
             {
-                gameBoard.Board = board;
+                ComputerMove();
             }
-            else
-            {
-                gameBoard.Board = new char[3, 3];
-            }
-
-            if (TempData.ContainsKey("CurrentPlayer") && TempData["CurrentPlayer"] is char currentPlayer)
-            {
-                gameBoard.CurrentPlayer = currentPlayer;
-            }
-            else
-            {
-                gameBoard.CurrentPlayer = 'X';
-            }
-
-
-            TempData["Board"] = gameBoard.Board;
-            TempData["CurrentPlayer"] = gameBoard.CurrentPlayer;
 
             return RedirectToAction("Index");
         }
 
-        private bool CheckForWinner(char[,] board, char player)
+        public IActionResult Reset()
         {
-            // Check rows, columns, and diagonals for a winner
-            for (int i = 0; i < 3; i++)
-            {
-                if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
-                    return true;
+            _game.Reset();
+            return RedirectToAction("Index");
+        }
 
-                if (board[0, i] == player && board[1, i] == player && board[2, i] == player)
-                    return true;
+        public IActionResult SetComputerPlayer(bool isComputerPlayer)
+        {
+            _game.IsComputerPlayer = isComputerPlayer;
+            _game.Reset();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult SetPlayerNames(string playerX, string playerO)
+        {
+            _game.PlayerX = playerX;
+            _game.PlayerO = playerO;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ResetScores()
+        {
+            _game.ResetScores();
+            return RedirectToAction("Index");
+        }
+
+        private void ComputerMove()
+        {
+            int bestScore = int.MinValue;
+            int bestMove = -1;
+
+            for (int i = 0; i < _game.Board.Length; i++)
+            {
+                if (_game.Board[i] == null)
+                {
+                    _game.Board[i] = "O";
+                    int score = Minimax(_game.Board, 0, false);
+                    _game.Board[i] = null;
+
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        bestMove = i;
+                    }
+                }
             }
 
-            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
-                return true;
+            _game.MakeMove(bestMove);
+        }
 
-            if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
-                return true;
+        private int Minimax(string[] board, int depth, bool isMaximizing)
+        {
+            string winner = CheckWinner(board);
+            if (winner != null)
+            {
+                return Score(winner);
+            }
 
-            return false;
+            if (isMaximizing)
+            {
+                int bestScore = int.MinValue;
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] == null)
+                    {
+                        board[i] = "O"; // Computer's move
+                        int score = Minimax(board, depth + 1, false);
+                        board[i] = null;
+                        bestScore = Math.Max(score, bestScore);
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] == null)
+                    {
+                        board[i] = "X"; // Human's move
+                        int score = Minimax(board, depth + 1, true);
+                        board[i] = null;
+                        bestScore = Math.Min(score, bestScore);
+                    }
+                }
+                return bestScore;
+            }
+        }
+
+        private string CheckWinner(string[] board)
+        {
+            int[][] lines = new int[][]
+            {
+                new int[] { 0, 1, 2 },
+                new int[] { 3, 4, 5 },
+                new int[] { 6, 7, 8 },
+                new int[] { 0, 3, 6 },
+                new int[] { 1, 4, 7 },
+                new int[] { 2, 5, 8 },
+                new int[] { 0, 4, 8 },
+                new int[] { 2, 4, 6 },
+            };
+
+            foreach (var line in lines)
+            {
+                if (board[line[0]] != null &&
+                    board[line[0]] == board[line[1]] &&
+                    board[line[0]] == board[line[2]])
+                {
+                    return board[line[0]];
+                }
+            }
+
+            if (Array.TrueForAll(board, b => b != null))
+            {
+                return "Draw";
+            }
+
+            return null;
+        }
+
+        private int Score(string winner)
+        {
+            if (winner == "O") return 1;
+            if (winner == "X") return -1;
+            return 0;
         }
     }
 }
-
